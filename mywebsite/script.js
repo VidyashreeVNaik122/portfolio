ea436371fc759d256120d4e9b549bcbd4b0a3998
VidyashreeVNaik122 <vidyashreev122@gmail.com>
2024-12-12 21:12:51 +0530

    Add files via upload

---
+++ mywebsite/script.js
@@ -0,0 +1,12 @@
+// Smooth scrolling for anchor links
+$(document).ready(function() {
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800);
+        }
+    });
+});
